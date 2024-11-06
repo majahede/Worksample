c127c5448f35e3f2818a6711d6a92d4a135637bb
Maja Hedegärd <majahedegard@gmail.com>
2024-11-06 20:20:51 +0100

    Read toll fee information from json

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using TollFeeCalculator;
+
+TollCalculator tollCalculator = new TollCalculator();
---
+++ source/Models/TollInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace TollFeeCalculator.Models
+{
+    public class TollInfo
+    {
+        public List<string> TollFreeDays { get; set; }
+        public List<int> TollFreeMonths { get; set; }
+        public List<DateTime> TollFreeDates { get; set; }
+    }
+}
